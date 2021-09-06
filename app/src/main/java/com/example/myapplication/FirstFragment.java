0e5670d77fd4db885153317f9cd10e26b5069f5a
duyle <leduy.av22@gmail.com>
2021-09-06 20:56:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FirstFragment extends Fragment {
+    TextView textView;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, container, false);
+        textView = view.findViewById(R.id.text);
+        textView.setText("first");
+        return view;
+    }
+}
